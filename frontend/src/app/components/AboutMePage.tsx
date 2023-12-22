2e0ca73e0f0813bb9eb1bccfbbf6473d15f6b25b
Seif <seifehab.bl4@gmail.com>
2023-12-22 01:22:18 +0000

    minor changes to about me template

--- frontend/src/app/components/AboutMePage.tsx
+++ frontend/src/app/components/AboutMePage.tsx
@@ -6,7 +6,6 @@ import NavBar from "./NavBar";
 const AboutMePage = () => {
     const text = [
         "non ad deserunt pariatur ullamco labore ullamco aute labore ad ut sunt amet excepteur et sunt aliqua Lorem deserunt eu commodo labore consectetur ipsum id ex consequat consequat consectetur enim cupidatat adipisicing aute sit consequat anim exercitation sint id ad id anim minim dolore reprehenderit reprehenderit veniam minim adipisicing deserunt laborum nulla reprehenderit nostrud adipisicing elit amet consectetur.",
-        "duis commodo aliqua non fugiat anim deserunt adipisicing sunt ipsum anim aliquip ea amet veniam enim Lorem mollit quis culpa dolore qui ipsum non minim anim consequat laborum ullamco quis sit ut cupidatat qui adipisicing exercitation Lorem occaecat do cupidatat ipsum aute",
     ];
     return (
         <div className="layout">
