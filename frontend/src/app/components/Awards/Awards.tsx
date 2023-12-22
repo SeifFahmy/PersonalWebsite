e97843d1f561ed4fc108841cea44f0e0cc543f0c
Seif <seifehab.bl4@gmail.com>
2023-12-22 16:24:42 +0000

    created awards component

---
+++ frontend/src/app/components/Awards/Awards.tsx
@@ -0,0 +1,35 @@
+import awards from "@/app/data/awards";
+import styles from "./Awards.module.css";
+
+const Awards = () => {
+    return (
+        <div className={styles.container}>
+            <h3>Awards</h3>
+            <p>
+                veniam est aute cillum commodo dolore mollit tempor aute
+                reprehenderit culpa laborum sunt consequat et adipisicing
+                consequat laborum sit ad
+            </p>
+            <table className={styles.table}>
+                <thead>
+                    <tr>
+                        <th>Award</th>
+                        <th>From</th>
+                        <th>No. of Wins</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    {awards.map((award, i) => (
+                        <tr key={i}>
+                            <td>{award.award}</td>
+                            <td>{award.from}</td>
+                            <td>{award.number}</td>
+                        </tr>
+                    ))}
+                </tbody>
+            </table>
+        </div>
+    );
+};
+
+export default Awards;
---
+++ frontend/src/app/components/AwardsPage.tsx
@@ -0,0 +1,13 @@
+import NavBar from "./NavBar";
+import Awards from "./Awards";
+
+const AwardsPage = () => {
+    return (
+        <div className="layout">
+            <NavBar />
+            <Awards />
+        </div>
+    );
+};
+
+export default AwardsPage;
