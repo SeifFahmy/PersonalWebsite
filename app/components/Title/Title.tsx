d7ca8c590472cbcdc635177fb6fb49ecb8e8eceb
Seif <seifehab.bl4@gmail.com>
2024-03-27 01:00:28 +0000

    updated title page design

--- app/components/Title/Title.tsx
+++ app/components/Title/Title.tsx
@@ -1,8 +1,26 @@
+"use client";
+
 import mouse from "@public/images/mouse.webp";
 import Image from "next/image";
 import styles from "./Title.module.css";
+import "animate.css";
+import { useEffect, useState } from "react";
 
 const Title = () => {
+    const [bounce, setBounce] = useState(true);
+    const bounceAnimation =
+        "animate__animated animate__bounce animate__slow animate__delay-3s";
+
+    useEffect(() => {
+        const intervalId = setInterval(() => {
+            if (window.scrollY < window.innerHeight / 2) {
+                setBounce((prev) => !prev);
+            }
+        }, 5000);
+
+        return () => clearInterval(intervalId);
+    }, []);
+
     return (
         <div className={styles.page}>
             <div className={styles.triangle} />
@@ -16,7 +34,10 @@ const Title = () => {
                 <span className={styles.header}>Seif Fahmy</span>
             </div>
             <Image
-                className={styles.scrollIcon}
+                className={`${styles.scrollIcon} ${
+                    bounce ? bounceAnimation : ""
+                }`}
+                // className={`${styles.scrollIcon}`}
                 src={mouse}
                 alt="scroll icon"
             />
